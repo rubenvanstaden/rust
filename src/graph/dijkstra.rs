659f891e8e2d7da5fc146697b89c8f87da1cf5c5
Ruben van Staden <rvs.cipher@proton.me>
2023-07-24 11:46:51 -0400

    basic working dijkstra from memory

--- src/graph/dijkstra.rs
+++ src/graph/dijkstra.rs
@@ -4,83 +4,85 @@ type Id = usize;
 type Edge = (Id, i32); // (node, weight)
 
 pub struct Graph {
-    pub nodes: HashMap<Id, Vec<Edge>>,
+    nodes: HashMap<Id, Vec<Edge>>,
 }
 
 impl Graph {
+
     pub fn new() -> Self {
         Graph {
             nodes: HashMap::new(),
         }
     }
 
-    pub fn dijkstra(&self, start: Id) -> (Vec<i32>, i32) {
-        let mut total_weight = 0;
-        let mut dist = i32::MAX;
+    pub fn dijkstra(&self, start: usize) -> (i32, Vec<i32>) {
 
-        let mut intree = vec![false; self.nodes.len()];
+        // Create a visited tree.
+        let mut visited = vec![false; self.nodes.len()];
+
+        // Keep track of the minimal distance each node has to the start.
         let mut distance = vec![i32::MAX; self.nodes.len()];
 
         distance[start] = 0;
 
-        // The node v is the current node.
         let mut v = start;
 
-        while !intree[v] {
+        while !visited[v] {
 
-            intree[v] = true;
+            println!("curr: {}", v);
 
-            if v != start {
-                total_weight = total_weight + dist;
-            }
+            // Add node to the visited tree.
+            visited[v] = true;
 
-            // 1. Update the distance of each neighbouring node.
-
+            // Update the distance to each neighbor.
             if let Some(neighbors) = self.nodes.get(&v) {
-                for &(u, weight) in neighbors {
-                    if distance[u] > distance[v] + weight {
-                        distance[u] = distance[v] + weight;
+                for (u, weight) in neighbors {
+                    println!("(u: {}, w: {})", u, weight);
+                    if distance[*u] > distance[v] + weight {
+                        distance[*u] = distance[v] + weight;
                     }
                 }
             }
 
-            // 2. Now, select the neighbor with the shortest path from "start".
+            // Extract the shortest path to the neighbors.
+            let mut dist = i32::MAX;
+            for nodes in self.nodes.values() {
+                for (u, _) in nodes {
+                    if !visited[*u] && dist > distance[*u] {
 
-            dist = i32::MAX;
-            for (&u, _edges) in &self.nodes {
-                if !intree[u] && dist > distance[u] {
-                    dist = distance[u];
-                    v = u;
+                        // Update inmem var to calculate the MIN.
+                        dist = distance[*u];
+
+                        // Update the current node to the shortest path found.
+                        v = *u;
+                    }
                 }
             }
         }
 
-        (distance, total_weight)
+        (0, distance)
     }
 }
 
 #[cfg(test)]
-mod tests {
+mod test {
     use super::*;
 
     #[test]
-    fn test_dijkstra() {
+    fn test() {
 
-        let mut g = Graph {
-            nodes: HashMap::new(),
-        };
+        let mut g = Graph::new();
 
         g.nodes.insert(0, vec![(1, 1), (3, 3)]);
         g.nodes.insert(1, vec![(0, 1), (2, 1)]);
         g.nodes.insert(2, vec![(1, 1), (3, 2)]);
         g.nodes.insert(3, vec![(0, 3), (2, 2)]);
 
-        let (distances, weight) = g.dijkstra(0);
+        let (weight, distance) = g.dijkstra(0);
 
-        assert_eq!(weight, 6);
-        assert_eq!(distances[0], 0);
-        assert_eq!(distances[1], 1);
-        assert_eq!(distances[2], 2);
-        assert_eq!(distances[3], 3);
+        assert_eq!(weight, 0);
+        assert_eq!(distance[0], 0);
+        assert_eq!(distance[1], 1);
+        assert_eq!(distance[2], 2);
     }
 }
